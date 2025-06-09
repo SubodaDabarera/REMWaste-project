5493cf65427edcb231cb49b9bfae3b69573a9e9d
SubodaDabarera <SubodaDabarera99@gmail.com>
2025-06-09 14:52:56 +0200

    skip card usage added

--- src/components/SkipPage.tsx
+++ src/components/SkipPage.tsx
@@ -1,6 +1,7 @@
 import useFetchSkips from "@/hooks/useFetchSkips";
 import type { Skip } from "@/types/Skip";
-import React, { useState } from "react";
+import { useState } from "react";
+import SkipCard from "@/components/SkipCard";
 
 const SKIP_API =
   "/api/y/ct/?l=XDl3Kn&m=8kKiA5Xs4lyuOBlr&b=LN4zdPOCY2wffjE5vH.B0w";
@@ -8,26 +9,26 @@ const SKIP_API =
 function SkipPage() {
   const { data: skips } = useFetchSkips(SKIP_API);
 
-  const [selectedSkip, setSelectedSkip] = useState();
+  const [selectedSkip, setSelectedSkip] = useState(0);
 
   const handleSkipSelect = (skip_id: number) => {
-    console.log("skip", skip_id);
+    setSelectedSkip(skip_id);
   };
 
   return (
     <div>
-      <h2 className="text-3xl font-bold">Select Skip</h2>
+      <h2 className="text-3xl font-bold">Select Your Skip</h2>
       <span>Select a skip that suits to your usecase</span>
 
       {/* Card container */}
-      <div className="grid justify-center grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 bg-yellow-300 w-full">
+      <div className="grid justify-center grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
         {skips.map((skip: Skip) => (
           <div
             key={skip.id}
-            className="bg-red-900 max-w-72 flex flex-col"
+            className="flex flex-col"
             onClick={() => handleSkipSelect(skip.id)}
           >
-            <span>Title</span>
+            <SkipCard selectedSkip={selectedSkip} data={skip} />
           </div>
         ))}
       </div>
