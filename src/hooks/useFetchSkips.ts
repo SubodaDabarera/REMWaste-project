c34312e016d10a5917858efcd34b2c84e400bb09
SubodaDabarera <SubodaDabarera99@gmail.com>
2025-06-10 14:48:55 +0200

    added loading state

--- src/hooks/useFetchSkips.ts
+++ src/hooks/useFetchSkips.ts
@@ -3,21 +3,25 @@ import React, { useEffect, useState } from "react";
 
 function useFetchSkips(url: string) {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     fetch();
   }, [url]);
 
   const fetch = async () => {
+    setLoading(true)
     const skipData = await axios.get(url);
     if (skipData.status == 200) {
       setData(skipData.data);
+      setLoading(false)
       return;
     }
     setData(skipData.data);
+    setLoading(false)
   };
 
-  return { data };
+  return { data, loading };
 }
 
 export default useFetchSkips;
